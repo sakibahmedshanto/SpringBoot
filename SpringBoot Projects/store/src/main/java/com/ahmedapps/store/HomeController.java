67752fc73372cc4f2da30ecd9800864c2e80dba1
sakibahmedshanto <sakibsanto57@gmail.com>
2025-07-22 18:50:57 +0600

    upto lecture 15 (auto restart of backedn and using @value to get app properties

--- SpringBoot Projects/store/src/main/java/com/ahmedapps/store/HomeController.java
+++ SpringBoot Projects/store/src/main/java/com/ahmedapps/store/HomeController.java
@@ -1,13 +1,18 @@
 package com.ahmedapps.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value(("${spring.application.name}"))
+    private String appName;
     @RequestMapping("/")
     public String index() {
         String viewName=getViewName();
+        System.out.println("View Name: " + viewName);
+        System.out.println("Application Name: " + appName);
         return viewName;
     }
     private String getViewName(){
